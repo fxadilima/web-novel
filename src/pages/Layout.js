70fb6243fee4b7983ac9af6e516521d82711694e
adilima <adi7598@gmail.com>
2022-09-26 14:40:58 +0700

    Basic layout decided

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import { Outlet, Link } from "react-router-dom";
 
 const Layout = () => {
     return (
         <>
             <div className="w3-top w3-bar w3-white w3-card">
-                <Link to='/' className="w3-bar-item w3-button w3-hover-red">Home</Link>
+                <Link to='/' className="w3-bar-item w3-button w3-hover-red"><i className="fas fa-home"></i></Link>
                 <Link to='/blogs' className="w3-bar-item w3-button w3-hover-red">Blogs</Link>
                 <Link to='/people' className="w3-bar-item w3-button w3-hover-red">People</Link>
             </div>
