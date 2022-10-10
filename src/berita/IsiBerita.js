53c17f4b198c73b888a821b37f898b73f4742c15
adilima <adi7598@gmail.com>
2022-10-10 10:41:56 +0700

    Fixing an unused reference that caused a warning (treated as error)

--- src/berita/IsiBerita.js
+++ src/berita/IsiBerita.js
@@ -8,7 +8,13 @@ const IsiBerita = () => {
         <div className="w3-container w3-padding-32">
             <h1>Isi Berita</h1>
             <p>
-                Cek situs di <a href="https://dunia-timur.netlify.app">Netlify</a>.
+                Cek situs di <a href="https://dunia-timur.netlify.app">Netlify</a>.<br/>
+            </p>
+            <p>
+                Netlify by default treating warning as error, kalau ada build failed cek di lokal
+                apakah ada salah satu reference yang tidak dipakai (akan mengakibatkan warning).
+                Ini sekedar link supaya reference yang ada dipakai:<br/>
+                <Link to="/berita">Berita</Link>
             </p>
             <Outlet />
         </div>
