6c9658fbaf449375d8d188f22579dcf048cdd40b
adilima <adi7598@gmail.com>
2022-10-10 10:48:31 +0700

    added 2 links into Sejarah.js

--- src/berita/Sejarah.js
+++ src/berita/Sejarah.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
 
 const Sejarah = () => {
     return (
         <div className="w3-container w3-padding-32">
             <h1>Sejarah</h1>
             <p>
-                Tulisan ini harus ada kalau kita akses /berita/sejarah.
+                Kalau sudah mendeklarasikan import, harus dipakai, karena Netlify secara default menganggap warning 
+                sebagai error. Agak malas mencari-cari setting untuk mengubahnya. Karena itu tombol ini ada di sini, toh nantinya kita 
+                akan memakai reference tersebut.
+            </p>
+            <p>
+                <div className="w3-bar">
+                    <Link className="w3-bar-item w3-btn" to="/berita/sejarah">Sejarah</Link>
+                    <Link className="w3-bar-item w3-btn" to="/people">People</Link>
+                </div>
             </p>
         </div>
     );
