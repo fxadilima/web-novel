a93a1d54e8e16ea8f026c36cb44d58221f44b921
adilima <adi7598@gmail.com>
2023-03-06 22:13:26 +0700

    update hello-edge

--- netlify/edge-functions/hello.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as Server from 'react-dom/server';
-
-const Greet = () => <h1>Hello, world!</h1>;
-export default () => new Response(Server.renderToString(<Greet/>));
-
-export const config = { 
-    path: "/hello-edge" 
-};
