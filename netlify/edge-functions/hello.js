b16fe874bfd534d1a3370fcc60b5d249874046d9
adilima <adi7598@gmail.com>
2023-03-07 09:20:55 +0700

    renamed test to hello-edge

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,20 +1,27 @@
 
-export default () => new Response(`
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Sejarah</title>
-        <link href="/styles/w3.css" rel="stylesheet">
-        <link rel="icon" href="/favicon.ico">
-        <script src="https://kit.fontawesome.com/acda0af544.js" crossorigin="anonymous"></script>
-    </head>
-    <body>
-        <h1>Hello Edge Function</h1>
-        <p>This page is the <strong>Response</strong>.</p>
-        <p><a href="/">Back</a></p>
-    </body>
-</html>`
-);
+export default (request, context) => {
+    const html = `
+    <html lang="en">
+        <head>
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <title>Sejarah</title>
+            <link href="/styles/w3.css" rel="stylesheet">
+            <link rel="icon" href="/favicon.ico">
+            <script src="https://kit.fontawesome.com/acda0af544.js" crossorigin="anonymous"></script>
+        </head>
+        <body>
+            <h1>Hello Edge Function</h1>
+            <p>This page is the <strong>Response</strong>.</p>
+            <p><a href="/">Back</a></p>
+        </body>
+    </html>`;
+    return new Response(html, {
+        status: 200,
+        headers: { 'Content-Type': 'text/html' },
+    });
+};
 
-export const config = { path: "/test" }
+export const config = { 
+    path: "/hello-edge" 
+};
