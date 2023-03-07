ec7a2ec19a86667f0e32cb01f5c43ccc1e7bd462
adilima <adi7598@gmail.com>
2023-03-07 23:36:56 +0700

    added external module 2

--- public/help/modules/main.js
+++ public/help/modules/main.js
@@ -1,8 +1,8 @@
 
 let elm = document.getElementById("maybe");
 elm.innerHTML = `
-<div class="w3-panel w3-card-4">
-  <div class="w3-container w3-blue w3-text-white">
+<div class="w3-container w3-card-4">
+  <div class="w3-panel w3-blue">
     <h3>Dynamic</h3>
   </div>
   <div class="w3-container">
