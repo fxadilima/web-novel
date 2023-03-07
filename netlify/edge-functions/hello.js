a599aa8a7d05def86c6ddb9e8ee830a3da928fcb
fxadilima <107688563+fxadilima@users.noreply.github.com>
2023-03-07 08:37:02 +0700

    Update hello.js

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,3 +1,10 @@
+export default () => new Response("Hello world")
+
+export const config = { path: "/test" }
+
+/**
+ * Looks like we are having trouble with these
+ *
 import * as React from 'react';
 import * as Server from 'react-dom/server';
 
@@ -7,4 +14,5 @@ export default () => new Response(Server.renderToString(<Greet/>));
 export const config = { 
     path: "/hello-edge" 
 };
+*/
 
