0c47f672a87183a8c1193e7539f14d3885a20ae0
adilima <adi7598@gmail.com>
2023-03-09 02:45:59 +0700

    trying deno14

--- public/help/modules/main.js
+++ public/help/modules/main.js
@@ -2,21 +2,26 @@
 //import { renderToString } from 'https://esm.sh/react-dom/server';
 
 
-let elm = document.getElementById("maybe");
-let mdxSrc = fetch("https://dunia-timur.netlify.app/help/alt/deno.mdx", {headers: {accept:'text/mdx'}})
-.then(x => x.text())
-.then((y) => { return y;});
+showIntro = async () => {
+  let elm = document.getElementById("maybe");
+  let mdxSrc = await fetch("https://dunia-timur.netlify.app/help/alt/deno.mdx");
 
-elm.innerHTML = `
-<div class="w3-container w3-card-4">
-  <div class="w3-panel w3-blue">
-    <h3>Dynamic</h3>
+  elm.innerHTML = `
+  <div class="w3-container w3-card-4">
+    <div class="w3-panel w3-blue">
+      <h3>Dynamic</h3>
+    </div>
+    <div class="w3-container">
+      <p>
+        Konten ini datang dari module eksternal.
+      </p>
+      <pre><code>${mdxSrc}</code></pre>
+    </div>
   </div>
-  <div class="w3-container">
-    <p>
-      Konten ini datang dari module eksternal.
-    </p>
-    <pre><code>${mdxSrc}</code></pre>
-  </div>
-</div>
-`;
+  `;
+
+}
+
+showIntro();
+
+
