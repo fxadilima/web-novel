f5280c70c944446f020379f7b0226aba7074759f
adilima <adi7598@gmail.com>
2023-03-09 01:52:21 +0700

    trying deno6

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -8,7 +8,7 @@ export default async (request, context) => {
         strSearch = tmp.toString();
         //const compiled = await compile(await fs.readFile('/mdx/EdgeFunction.mdx'));
         //strSearch = Server.renderToString(String(compiled));
-        mdxSrc = await fetch("./help/deno.mdx");
+        mdxSrc = await fetch("/help/deno.mdx");
         strSearch = "<div><pre><code>" + mdxSrc + "</code></pre></div>";
     }
     const html = `
