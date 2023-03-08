541f27830f71e4bf437ec7968f8111059a27af2b
adilima <adi7598@gmail.com>
2023-03-09 01:58:02 +0700

    trying deno8

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -8,7 +8,8 @@ export default async (request, context) => {
         strSearch = tmp.toString();
         //const compiled = await compile(await fs.readFile('/mdx/EdgeFunction.mdx'));
         //strSearch = Server.renderToString(String(compiled));
-        mdxSrc = await fetch("https://dunia-timur.netlify.app/help/deno.mdx");
+        mdxSrc = await fetch("https://dunia-timur.netlify.app/help/deno.mdx")
+        .then(x => x.text()).then((y) => { return y;});
         strSearch = "<div><pre><code>" + mdxSrc + "</code></pre></div>";
     }
     const html = `
