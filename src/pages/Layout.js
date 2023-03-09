743ec094260df9f8460bb2419b9313d1881906f8
adilima <adi7598@gmail.com>
2023-03-09 14:04:39 +0700

    adding button

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -8,6 +8,7 @@ const Layout = () => {
                 <Link to='/' className="w3-bar-item w3-button w3-hover-red"><i className="fas fa-home"></i></Link>
                 <Link to='/blogs' className="w3-bar-item w3-button w3-hover-red">Blogs</Link>
                 <Link to='/people' className="w3-bar-item w3-button w3-hover-red">People</Link>
+                <a className="w3-button w3-hover-red w3-bar-item" href="/hello-edge">Edge</a>
             </div>
             <Outlet />
         </>
